e5ee65e39281d230615bcbfa8fccfa72908c5e6f
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-07-27 16:11:10 +0530

    Time: 16 ms (74.57%), Space: 6 MB (31.51%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        int l = 0, r = num.length() - 1;
+        while(l < r) {
+            if(num[l] != num[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
