d917c43a11349e6b70228c1d63dcb0a9d428eebc
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-10-22 13:07:08 +0530

    Time: 8 ms (92.77%), Space: 12.9 MB (75.90%) - LeetHub

---
+++ 0517-super-washing-machines/0517-super-washing-machines.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMinMoves(vector<int>& machines) {
+        int totalDresses = 0, n = machines.size();
+        for(auto idx = 0 ; idx < n ; idx++)
+            totalDresses += machines[idx];
+        if(0 != totalDresses % n)
+            return -1;
+        
+        auto targetDresses = totalDresses / n, totalMoves = 0, balance = 0;
+        for(auto idx = 0 ; idx < n ; idx++) {
+            balance += machines[idx] - targetDresses;
+            totalMoves = max(totalMoves, max(machines[idx] - targetDresses, abs(balance)));
+        }
+        
+        return totalMoves;
+    }
+};
