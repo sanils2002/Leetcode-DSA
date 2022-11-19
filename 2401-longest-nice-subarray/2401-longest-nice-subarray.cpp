405a5d59baa04a6ea38b87ea0740892208510b89
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-11-19 13:33:37 +0530

    Time: 475 ms (25.23%), Space: 57 MB (93.53%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int mask = 0, fast = 0, slow = 0, mx = 0;
         while(fast < nums.size()) {
             while(0 != (mask & nums[fast]))
-                mask ^= nums[slow], slow++;
+                mask ^= nums[slow++];
             mask |= nums[fast];
             mx = max(mx, fast - slow + 1);
             fast++;
