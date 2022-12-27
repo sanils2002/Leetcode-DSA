5543270758243067e9d73fdf79d6f3aefafbaad1
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-12-27 13:04:56 +0530

    Added solution - LeetHub

---
+++ Heap Sort - GFG/heap-sort.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// C++ program for implementation of Heap Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted 
+// in increasing order when heapSort() is called.
+
+class Solution
+{
+    public:
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i) {
+      int largest = i; //Initialize largest as root
+      int l = 2 * i + 1; //left = 2*i + 1
+      int r = 2 * i + 2; //right = 2*i + 2
+      
+      //If left child is larger than root
+      if(l < n && arr[l] > arr[largest]) {
+          largest = l;
+      }
+      
+      //If right child is larger than largest so far
+      if(r < n && arr[r] > arr[largest]) {
+          largest = r;
+      }
+      
+      //If largest is not root
+    if(largest != i) {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+    }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)  
+    { 
+        //Index of last non-leaf node
+        int startIdx = (n / 2) - 1;
+        
+        //Perform reverse level order traversal
+        //from last non-leaf node and heapify
+        //each node
+        for(int i = startIdx; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n)
+    {
+        //Build heap (rearrange array)
+        buildHeap(arr, n);
+        
+        //One by one extract an element from heap
+        for(int i = n - 1; i >= 0; i--) {
+            //Move current root to end
+            swap(arr[0], arr[i]);
+            
+            //call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
