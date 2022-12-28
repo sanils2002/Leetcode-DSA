1096c24491991d3769f72e89185aa749934b5577
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-12-28 09:09:58 +0530

    Time: 757 ms (67.07%), Space: 105 MB (55.34%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-        // Build min heap with '-' sign.
-  std::priority_queue<int, std::vector<int>, std::greater<int>> heap;
-  for (int num : piles) {
-    heap.push(-num);
-  }
+        //Build min heap with '-' sign.
+        priority_queue<int, vector<int>, greater<int>> heap;
+        for(int num : piles) {
+            heap.push(-num);
+        }
 
-  // Iterate for k times and perform pile[i]//2.
-  for (int i = 0; i < k; i++) {
-    int curr = -heap.top();
-    heap.pop();
-    int remove = std::ceil(curr / 2.0);
-    heap.push(-remove);
-  }
+        //Iterate for k times and perform pile[i]//2.
+        for(int i = 0; i < k; i++) {
+            int curr = -heap.top();
+            heap.pop();
+            int remove = std::ceil(curr / 2.0);
+            heap.push(-remove);
+        }
 
-  // Now return ans by putting them back into '+'.
-  int total = 0;
-  while (!heap.empty()) {
-    total -= heap.top();
-    heap.pop();
-  }
-  return total;
+        //Now return ans by putting them back into '+'.
+        int total = 0;
+        while(!heap.empty()) {
+            total -= heap.top();
+            heap.pop();
+        }
+  
+        return total;
     }
 };
