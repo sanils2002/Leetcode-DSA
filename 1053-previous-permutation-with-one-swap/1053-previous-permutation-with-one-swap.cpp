f4b6509faff87278699333b6cbe44aa252cdf472
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2022-12-29 20:58:31 +0530

    Time: 36 ms (69.78%), Space: 24.8 MB (49.33%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> prevPermOpt1(vector<int>& arr) {
+        int n = arr.size();
+  int i = n - 2;
+  while (i >= 0 && arr[i] <= arr[i + 1]) i--;
+  if (i < 0) return arr;
+
+  int j = i + 1;
+  while (j < n && arr[j] < arr[i]) j++;
+  j--;
+
+  while (j > i + 1 && arr[j - 1] == arr[j]) j--;
+
+  std::swap(arr[i], arr[j]);
+  return arr;
+    }
+};
