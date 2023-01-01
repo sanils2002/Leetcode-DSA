f080d19ea7ffc1328e63c3acaff7a42adf3648b0
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-01 07:56:22 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -2,37 +2,42 @@ class Solution {
 public:
     bool wordPattern(string p, string s) {
         // Split s into words.
-    vector<string> words;
-    int i = 0;
-    while (i < s.size()) {
-        int j = i + 1;
-        while (j < s.size() && s[j] != ' ') j++;
-        words.push_back(s.substr(i, j - i));
-        i = j + 1;
-    }
-
-    // Check if the number of words in s is the same as the number of letters in p.
-    if (words.size() != p.size()) return false;
-
-    // Map each letter in p to a word in s.
-    unordered_map<char, string> m;
-    for (int i = 0; i < p.size(); i++) {
-        if (m.count(p[i]) == 0) {
-            // If p[i] is not in the map, add it.
-            m[p[i]] = words[i];
-        } else {
-            // If p[i] is already in the map, check if it maps to the same word as before.
-            if (m[p[i]] != words[i]) return false;
+        vector<string> words;
+        int i = 0;
+        while(i < s.size()) {
+            int j = i + 1;
+            while(j < s.size() && s[j] != ' ') 
+                j++;
+            words.push_back(s.substr(i, j - i));
+            i = j + 1;
         }
-    }
 
-    // Check if there are any two different letters in p that map to the same word.
-    for (int i = 0; i < p.size(); i++) {
-        for (int j = i + 1; j < p.size(); j++) {
-            if (p[i] != p[j] && m[p[i]] == m[p[j]]) return false;
+        // Check if the number of words in s is the same as the number of letters in p.
+        if(words.size() != p.size()) 
+            return false;
+
+        // Map each letter in p to a word in s.
+        unordered_map<char, string> m;
+        for(int i = 0; i < p.size(); i++) {
+            if(m.count(p[i]) == 0) {
+                // If p[i] is not in the map, add it.
+                m[p[i]] = words[i];
+            } 
+            else {
+                // If p[i] is already in the map, check if it maps to the same word as before.
+                if(m[p[i]] != words[i]) 
+                    return false;
+            }
         }
-    }
 
-    return true;
+        // Check if there are any two different letters in p that map to the same word.
+        for (int i = 0; i < p.size(); i++) {
+            for (int j = i + 1; j < p.size(); j++) {
+                if (p[i] != p[j] && m[p[i]] == m[p[j]]) 
+                    return false;
+            }
+        }
+
+        return true;
     }
 };
