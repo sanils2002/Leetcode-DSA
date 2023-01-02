76c40b2b686da3513051aec6017799c44d45a416
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-02 11:09:25 +0530

    Time: 5 ms (24.37%), Space: 6.2 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool detectCapitalUse(string w) {
+        int up = 0, low = 0;
+        for (char c : w) {
+            if (isupper(c)) {
+                up++;
+            } 
+            else {
+                low++;
+            }
+        }
+        return up == w.size() || low == w.size() || (up == 1 && isupper(w[0]));
+    }
+};
