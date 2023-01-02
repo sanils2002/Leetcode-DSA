95ecdb8335d4a9c33d2af7b64ce245fa20645654
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-02 14:51:53 +0530

    Time: 3 ms (68.94%), Space: 7.7 MB (8.09%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,59 +1,62 @@
 class Solution {
 private:
-    std::vector<std::string> split(std::string str) {
-  std::vector<std::string> words;
-  std::string word = "";
-  for (char c : str) {
-    if (c == ' ') {
-      words.push_back(word);
-      word = "";
-    } else {
-      word += c;
+    vector<string> split(std::string str) {
+        vector<std::string> words;
+        string word = "";
+        
+        for(char c : str) {
+            if(c == ' ') {
+                words.push_back(word);
+                word = "";
+            } 
+            else {
+                word += c;
+            }
+        }
+        
+        words.push_back(word);
+        return words;
     }
-  }
-  words.push_back(word);
-  return words;
-}
 
-std::string join(std::vector<std::string> words) {
-  std::string str = "";
-  for (int i = 0; i < words.size(); i++) {
-    str += words[i];
-    if (i < words.size() - 1) {
-      str += " ";
+    string join(std::vector<std::string> words) {
+        string str = "";
+        for(int i = 0; i < words.size(); i++) {
+            str += words[i];
+            if(i < words.size() - 1) {
+                str += " ";
+            }
+        }
+        return str;
     }
-  }
-  return str;
-}
-
 public:
     string capitalizeTitle(string title) {
         // If the string is empty, return an empty string
-  if (title.empty()) {
-    return "";
-  }
+        if(title.empty()) {
+            return "";
+        }
 
-  // Split the string into words
-  std::vector<std::string> words = split(title);
+        // Split the string into words
+        vector<string> words = split(title);
 
-  // Capitalize each word
-  for (int i = 0; i < words.size(); i++) {
-    std::string word = words[i];
+        // Capitalize each word
+        for(int i = 0; i < words.size(); i++) {
+            string word = words[i];
 
-    if (word.length() <= 2) {
-      // Change all letters to lowercase
-      std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-    } else {
-      // Change first letter to uppercase and remaining letters to lowercase
-      word[0] = toupper(word[0]);
-      std::transform(word.begin() + 1, word.end(), word.begin() + 1, ::tolower);
-    }
+            if (word.length() <= 2) {
+                // Change all letters to lowercase
+                transform(word.begin(), word.end(), word.begin(), ::tolower);
+            } 
+            else {
+                // Change first letter to uppercase and remaining letters to lowercase
+                word[0] = toupper(word[0]);
+                transform(word.begin() + 1, word.end(), word.begin() + 1, ::tolower);
+            }
 
-    words[i] = word;
-  }
+            words[i] = word;
+        }
 
-  // Join the words back into a single string
-  std::string capitalizedTitle = join(words);
-  return capitalizedTitle;
+        // Join the words back into a single string
+        string capitalizedTitle = join(words);
+        return capitalizedTitle;
     }
 };
