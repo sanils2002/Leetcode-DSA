7aff1fc7adf4790de0e7ec4783db0ac19bdf254c
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-04 22:14:03 +0530

    Time: 859 ms (27.55%), Space: 10.4 MB (69.25%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
         vector<long int> ugly(n, INT_MAX);
-    ugly[0] = 1;
-    vector<int> indices(primes.size(), 0);
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < primes.size(); j++) {
-            ugly[i] = min(ugly[i], primes[j] * ugly[indices[j]]);
-        }
-        for (int j = 0; j < primes.size(); j++) {
-            if (ugly[i] == primes[j] * ugly[indices[j]]) {
-                indices[j]++;
+        ugly[0] = 1;
+        vector<int> indices(primes.size(), 0);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < primes.size(); j++) {
+                ugly[i] = min(ugly[i], primes[j] * ugly[indices[j]]);
+            }
+            for(int j = 0; j < primes.size(); j++) {
+                if(ugly[i] == primes[j] * ugly[indices[j]]) {
+                    indices[j]++;
+                }
             }
         }
-    }
-    return ugly[n - 1];
+    
+        return ugly[n - 1];
     }
 };
