90cc9b6a18605650907054e939c3e79352f7a1f8
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-04 22:15:02 +0530

    Time: 427 ms (63.93%), Space: 10.3 MB (80.67%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -18,3 +18,9 @@ public:
         return ugly[n - 1];
     }
 };
+
+/*This program first initializes a vector ugly of size n with the maximum possible value of an integer (INT_MAX). It sets the first element of ugly to 1, which is the first super ugly number. It also initializes a vector indices of size primes.size() with all 0s, which will be used to keep track of the current index of each prime number in the ugly array.
+
+The program then enters a loop that runs n - 1 times, since the first element of ugly has already been set to 1. In each iteration of the loop, it finds the minimum of all products of a prime number in primes with an element in ugly and sets that value to the current element of ugly. It then increments the index of any prime number that was used to calculate this minimum value.
+
+Finally, the program returns the n - 1th element of ugly, which is the nth super ugly number.*/
