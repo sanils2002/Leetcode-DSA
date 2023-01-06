0f5aadb97ade767ce3b83b8ac6ec6575b5d2f03a
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-06 13:24:57 +0530

    Time: 2 ms (56.23%), Space: 5.9 MB (86.93%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.cpp
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int monotoneIncreasingDigits(int n) {
        // Convert the number to a string for easier manipulation
-  std::string str = std::to_string(n);
+        string str = to_string(n);
 
-  // Iterate through the digits of the number, starting from the second-to-last digit
-  for (int i = str.size() - 2; i >= 0; i--) {
-    // If the current digit is greater than the next digit, decrement the current digit and set all the digits after it to 9
-    if (str[i] > str[i + 1]) {
-      str[i]--;
-      for (int j = i + 1; j < str.size(); j++) {
-        str[j] = '9';
-      }
-    }
-  }
+        // Iterate through the digits of the number, starting from the second-to-last digit
+        for(int i = str.size() - 2; i >= 0; i--) {
+            // If the current digit is greater than the next digit, 
+            // decrement the current digit and set all the digits after it to 9
+            if(str[i] > str[i + 1]) {
+                str[i]--;
+                for(int j = i + 1; j < str.size(); j++) {
+                    str[j] = '9';
+                }
+            }
+        }
 
-  // Convert the modified string back to an integer and return it
-  return std::stoi(str);
+        // Convert the modified string back to an integer and return it
+        return stoi(str);
     }
 };
