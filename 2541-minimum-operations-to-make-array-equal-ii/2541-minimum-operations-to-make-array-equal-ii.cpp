440739e85fe067c7373c2623078d1dd7990b9988
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-25 21:19:29 +0530

    Time: 190 ms (53.97%), Space: 73.1 MB (23.81%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     long long minOperations(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<long long> DIFF(nums1.size());
-        if(nums1 == nums2) {
-            return 0;
-        }
-        for(long long i = 0; i < nums1.size(); i++) {
-            DIFF[i] = nums1[i] - nums2[i];
-        }
-
-        if(accumulate(DIFF.begin(), DIFF.end(), 0) != 0) {
-            return -1;
-        }
-        
-        long long res = 0;
-        for(long long i : DIFF) {
-            if(i > 0) {
-                res += i;
+        long long small=0;
+        long long large=0;
+        for(int i = 0 ; i < nums1.size() ; i++) {   
+            if(nums1[i] > nums2[i]) {   
+                if(k == 0) 
+                    return -1;
+                if((nums1[i] - nums2[i]) < k or (nums1[i] - nums2[i])%k != 0) 
+                    return -1;
+                large += (nums1[i] - nums2[i]) / k;
+                
+            }
+            else if(nums1[i] < nums2[i]) {  
+                if(k == 0) 
+                    return -1;
+                if((nums2[i] - nums1[i]) < k or (nums2[i] - nums1[i])%k != 0) 
+                    return -1;
+                small += (nums2[i] - nums1[i]) / k;
             }
         }
-
-        if(k != 0 && res % k == 0) {
-            return res / k;
-        } 
-        else {
+        
+        if(large != small) 
             return -1;
-        }
+        return large;
     }
 };
