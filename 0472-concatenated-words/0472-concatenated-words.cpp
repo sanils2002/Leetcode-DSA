6ec5cf43eeed9482fe9d48afd6994d5d545a9a51
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-27 08:35:43 +0530

    Time: 288 ms (60.90%), Space: 57.3 MB (45.63%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,34 +1,36 @@
 class Solution {
 private:
     // Function to check if a word is formed by concatenating other words
-bool isConcatenated(string word, unordered_set<string>& words) {
-    int n = word.length();
-    vector<int> dp(n + 1);
-    dp[0] = 1;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (dp[j] && words.count(word.substr(j, i - j))) {
-                dp[i] = 1;
-                break;
+    bool isConcatenated(string word, unordered_set<string>& words) {
+        int n = word.length();
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && words.count(word.substr(j, i - j))) {
+                    dp[i] = 1;
+                    break;
+                }
             }
         }
-    }
-    return dp[n];
+    
+        return dp[n];
 }
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> dict;
-    vector<string> result;
-    for (string word : words) {
-        dict.insert(word);
-    }
-    for (string word : words) {
-        dict.erase(word);
-        if (isConcatenated(word, dict)) {
-            result.push_back(word);
+        vector<string> result;
+        for(string word : words) {
+            dict.insert(word);
         }
-        dict.insert(word);
-    }
-    return result;
+        for(string word : words) {
+            dict.erase(word);
+            if(isConcatenated(word, dict)) {
+                result.push_back(word);
+            }
+            dict.insert(word);
+        }
+    
+        return result;
     }
 };
