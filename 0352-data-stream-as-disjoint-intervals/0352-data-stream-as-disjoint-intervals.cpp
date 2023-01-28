7e5d35b05d9d08313b0708ab5e16e3e98bcc3354
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-28 10:08:12 +0530

    Time: 130 ms (25.88%), Space: 48.7 MB (5.10%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -10,13 +10,15 @@ public:
         int n = intervals.size();
         vector<vector<int>> res;
         int l = value, r = value;
-        for (int i = 0; i < n; i++) {
-            if (intervals[i][1] + 1 < l) {
+        for(int i = 0; i < n; i++) {
+            if(intervals[i][1] + 1 < l) {
                 res.push_back(intervals[i]);
-            } else if (r + 1 < intervals[i][0]) {
+            } 
+            else if(r + 1 < intervals[i][0]) {
                 res.push_back({l, r});
                 l = intervals[i][0], r = intervals[i][1];
-            } else {
+            } 
+            else {
                 l = min(l, intervals[i][0]);
                 r = max(r, intervals[i][1]);
             }
