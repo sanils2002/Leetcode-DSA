52564158bb7a88a8c3a5a9d9daf7003cca925071
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-01-31 22:03:49 +0530

    Time: 36 ms (49.33%), Space: 9 MB (90.75%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> combination(k, 0);
+        int i = 0;
+        while(i >= 0) {
+            combination[i]++;
+            if(combination[i] > n) {
+                i--;
+            } 
+            else if (i == k - 1) {
+                res.push_back(combination);
+            } 
+            else {
+                i++;
+                combination[i] = combination[i - 1];
+            }
+        }
+    
+        return res;
+    }
+};
