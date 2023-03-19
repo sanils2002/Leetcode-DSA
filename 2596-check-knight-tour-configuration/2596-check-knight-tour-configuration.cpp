f6274d38125b4b4256ccac8d52eb963c342ac665
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-03-19 09:49:43 +0530

    Time: 20 ms (66.67%), Space: 28.3 MB (33.33%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,20 +1,22 @@
 class Solution {
 private:
     bool dfs(vector<vector<int>>& grid, int i, int j, int num_visited) {
-    int n = grid.size();
-    if (num_visited == n * n) {
-        return true;
-    }
-    if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != num_visited) {
-        return false;
-    }
-    vector<pair<int, int>> moves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-    for (auto [di, dj] : moves) {
-        if (dfs(grid, i + di, j + dj, num_visited + 1)) {
+        int n = grid.size();
+        if(num_visited == n * n) {
             return true;
         }
-    }
-    return false;
+        if(i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != num_visited) {
+            return false;
+        }
+    
+        vector<pair<int, int>> moves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+        for(auto [di, dj] : moves) {
+            if(dfs(grid, i + di, j + dj, num_visited + 1)) {
+                return true;
+            }
+        }
+    
+        return false;
 }
 public:
     bool checkValidGrid(vector<vector<int>>& grid) {
