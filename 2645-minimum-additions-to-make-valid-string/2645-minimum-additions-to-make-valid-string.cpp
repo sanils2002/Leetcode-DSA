3b70df4f846a69aec81c1b8b7e87e749b6a05331
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-04-16 13:19:15 +0530

    Time: 7 ms (57.14%), Space: 6.2 MB (64.29%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int addMinimum(string word) {
-        int n=word.length(); 
-        int ans=0; 
-        int i=0; 
-        while(i<n){ 
-            string s1=word.substr(i,3); 
-            string s2=word.substr(i,2); 
+        int n = word.length(); 
+        int ans = 0; 
+        int i = 0; 
+        while(i < n){ 
+            string s1 = word.substr(i, 3); 
+            string s2 = word.substr(i, 2); 
 
-            if(s1=="abc"){ 
-              i+=3; 
+            if(s1 == "abc") { 
+              i += 3; 
             } 
-            else if(s2=="ab" or s2=="ac" or s2=="bc"){ 
-                ans+=1; 
+            else if(s2 == "ab" or s2 == "ac" or s2 == "bc") { 
+                ans += 1; 
                 i+=2; 
             } 
-            else{ 
-                ans+=2; 
-                i+=1; 
+            else { 
+                ans += 2; 
+                i += 1; 
             } 
         }
+        
         return ans; 
     }
 };
