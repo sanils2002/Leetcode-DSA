c6962dccdba1e3e1cedeaca39f4969933be85351
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-05-08 12:11:55 +0530

    Time: 17 ms (78.65%), Space: 6.8 MB (45.13%) - LeetHub

---
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int n = s.size();
+    int cnt = 0;
+    for (int i = 0, j = n - 1; i < j; ) {
+        if (s[i] == s[j]) {
+            i++; j--;
+        } else {
+            int k = j - 1;
+            while (k > i && s[k] != s[i]) k--;
+            if (k == i) {
+                swap(s[i], s[i+1]);
+                cnt++;
+            } else {
+                while (k < j) {
+                    swap(s[k], s[k+1]);
+                    k++; cnt++;
+                }
+                i++; j--;
+            }
+        }
+    }
+    return cnt;
+    }
+};
