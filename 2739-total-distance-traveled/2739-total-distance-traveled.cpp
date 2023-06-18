40896ccca2d86d6ea51f33021047b200ed4c7f7a
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-06-18 23:26:28 +0530

    Time: 28 ms (22.22%), Space: 6 MB (22.22%) - LeetHub

--- 2739-total-distance-traveled/2739-total-distance-traveled.cpp
+++ 2739-total-distance-traveled/2739-total-distance-traveled.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int distanceTraveled(int mainTank, int additionalTank) {
         int maxDistance = mainTank * 10; // Calculate maximum distance based on fuel in the main tank
-    int remainingFuel = mainTank; // Initialize remaining fuel in the main tank
+        int remainingFuel = mainTank; // Initialize remaining fuel in the main tank
 
-    while (remainingFuel >= 5) {
-        if (additionalTank >= 1) {
-            remainingFuel -= 5; // Deduct 5 liters from the main tank
-            additionalTank -= 1; // Deduct 1 liter from the additional tank
-            remainingFuel += 1; // Transfer 1 liter from the additional tank to the main tank
-            maxDistance += 10; // Increase the maximum distance by 10 km
-        } else {
-            break; // Break the loop if there is not enough fuel in the additional tank
+        while(remainingFuel >= 5) {
+            if(additionalTank >= 1) {
+                remainingFuel -= 5; // Deduct 5 liters from the main tank
+                additionalTank -= 1; // Deduct 1 liter from the additional tank
+                remainingFuel += 1; // Transfer 1 liter from the additional tank to the main tank
+                maxDistance += 10; // Increase the maximum distance by 10 km
+            } 
+            else {
+                break; // Break the loop if there is not enough fuel in the additional tank
+            }
         }
-    }
 
-    return maxDistance;
+        return maxDistance;
     }
 };
