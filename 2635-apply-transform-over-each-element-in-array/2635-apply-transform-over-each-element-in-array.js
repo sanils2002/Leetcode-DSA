5965c4e2419b5d7d7491c48cbdc325fd843cf041
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-06-22 21:57:04 +0530

    Time: 53 ms (84.68%), Space: 41.4 MB (95.01%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    return arr.reduce((transformedArr, element, index) => {
+    transformedArr[index] = fn(element, index);
+    return transformedArr;
+  }, []);
+};
