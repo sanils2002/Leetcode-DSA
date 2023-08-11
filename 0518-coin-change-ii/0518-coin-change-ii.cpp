a131d686e0277c64b3429a358a32c6b1dfe18fc2
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-08-11 08:57:53 +0530

    Time: 8 ms (92.00%), Space: 7.1 MB (90.15%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+    dp[0] = 1; // Base case: 1 combination to make amount 0 (using no coins)
+
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; ++i) {
+            dp[i] += dp[i - coin];
+        }
+    }
+
+    return dp[amount];
+    }
+};
