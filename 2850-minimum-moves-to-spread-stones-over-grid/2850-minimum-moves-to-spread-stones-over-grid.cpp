13c76feacae99fc5cc2bf937400c96838f1f1515
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-09-10 09:44:12 +0530

    Time: 4 ms (100.00%), Space: 18.6 MB (85.71%) - LeetHub

---
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    int ans = 1e9;
+    void solve(vector<vector<int>> &grid , int idx , int cnt , int n , int m){
+        if(idx >= 9) {
+            int flag = 1;
+            for(int i = 0;i<3;i++) {
+                for(int j = 0;j<3;j++)  {
+                    if(grid[i][j] != 1) 
+                        flag = false;
+                }
+            }
+            if(flag) {
+                ans = min(ans , cnt);
+                return;
+            } 
+        }
+        
+
+        int x = idx / 3 , y = idx % 3;
+        if(grid[x][y] == 0) {
+            for(int i = 0 ; i < 3 ; i++) {
+                for(int j = 0 ; j < 3 ; j++)  {
+                    if(grid[i][j] > 1) {
+                        grid[i][j] -= 1;
+                        grid[x][y] += 1;
+                        solve(grid, idx + 1, cnt + abs(x-i) + abs(y-j) , n , m);
+                        grid[x][y] -= 1;
+                        grid[i][j] += 1;
+                    }
+                }
+            }
+        }
+        
+        else 
+            solve(grid, idx + 1, cnt, n, m);
+    }
+public:
+    int minimumMoves(vector<vector<int>>& grid) {
+        solve(grid, 0, 0, 3, 3);
+        return ans;
+    }
+};
