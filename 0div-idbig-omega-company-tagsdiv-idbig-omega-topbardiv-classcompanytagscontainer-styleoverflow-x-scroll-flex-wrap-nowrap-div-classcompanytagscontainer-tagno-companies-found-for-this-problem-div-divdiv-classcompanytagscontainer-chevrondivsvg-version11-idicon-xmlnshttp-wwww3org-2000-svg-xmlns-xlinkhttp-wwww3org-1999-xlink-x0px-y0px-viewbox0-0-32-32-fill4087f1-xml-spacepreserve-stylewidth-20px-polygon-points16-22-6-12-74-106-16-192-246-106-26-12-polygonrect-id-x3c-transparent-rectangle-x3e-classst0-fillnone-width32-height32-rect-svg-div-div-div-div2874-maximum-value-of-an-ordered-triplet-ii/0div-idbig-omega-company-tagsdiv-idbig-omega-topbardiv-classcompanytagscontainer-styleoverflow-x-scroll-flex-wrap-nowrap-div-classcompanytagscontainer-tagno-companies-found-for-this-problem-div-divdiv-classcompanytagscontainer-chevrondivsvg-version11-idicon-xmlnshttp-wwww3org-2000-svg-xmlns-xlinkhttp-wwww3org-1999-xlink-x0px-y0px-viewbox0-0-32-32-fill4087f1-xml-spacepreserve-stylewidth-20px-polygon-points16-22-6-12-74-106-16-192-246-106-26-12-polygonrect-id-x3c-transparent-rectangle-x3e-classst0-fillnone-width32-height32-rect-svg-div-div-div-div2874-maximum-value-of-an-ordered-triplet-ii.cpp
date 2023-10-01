2413a07a935375995b7e30f5d1575f9c1143aa72
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-10-01 09:58:03 +0530

    Time: 138 ms (66.67%), Space: 100 MB (66.67%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2874-maximum-value-of-an-ordered-triplet-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2874-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+typedef long long ll;
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        ll mx = 0;
+        vector<long long> pref(n, 0), suff(n, 0);
+        pref[0] = nums[0], suff[n - 1] = nums[n - 1];
+        
+        for(int i = 1 ; i < n ; i++)
+            pref[i] = max(pref[i - 1], (ll)nums[i]);
+        
+         for(int i = n - 2 ; i >= 0 ; i--)
+            suff[i] = max(suff[i + 1], (ll)nums[i]);
+        
+        for(int i = 1 ; i < n - 1 ; i++) {
+            ll l = pref[i - 1], r = suff[i + 1];
+            mx = max(mx, ((ll) l - (ll)nums[i]) * (ll)r);
+        }
+        
+        return mx;
+    }
+};
